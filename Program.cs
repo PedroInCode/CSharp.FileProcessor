61fefaf508bed51ffe1684b3965ec34434a43081
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-06 18:51:23 -0300

    setup inicial do projeto de manipulação de arquivos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+        // public override int Read(byte[] array, int offset, int count);
+
+
+        Console.ReadLine();
+    }
+
+    private static object ContaCorrente(int v1, int v2)
+    {
+        throw new NotImplementedException();
+    }
+}
