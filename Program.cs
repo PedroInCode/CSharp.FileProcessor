8c1a9c446ad4f4c0836c712736d47e1be5f4776e
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-07 16:24:01 -0300

    tratando excessoes com using

--- Program.cs
+++ Program.cs
@@ -6,27 +6,26 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1; 
-
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-        var buffer = new byte[1024]; // 1 KB
-
-        while (numeroDeBytesLidos != 0)
+        
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            var numeroDeBytesLidos = -1;
+
+            var buffer = new byte[1024]; // 1 KB
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
             EscreverBuffer(buffer);
+            // public override int Read(byte[] array, int offset, int count);
+
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
         }
-
-
-        /* Develuções:
-         * O número total de bytes lidos do buffer. Isso poderá ser menor que o número de bytes solicitado se esse numero de bytes não 
-         * estiver disponivel no momento, ou zero, se o final do fluxo for atingido. */
-
-        EscreverBuffer(buffer);
-        // public override int Read(byte[] array, int offset, int count);
-
-        Console.ReadLine();
     }
 
     static void EscreverBuffer(byte[] buffer)
