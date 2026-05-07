de81b4a71339ba4b3b9b85c028ee29d559dcbb16
Pedro Gustavo <pedrosystems.dev@gmail.com>
2026-05-07 16:09:33 -0300

    decodificar arquivo

--- Program.cs
+++ Program.cs
@@ -1,28 +1,47 @@
 ﻿using ByteBank;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1; 
+
         var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-        // public override int Read(byte[] array, int offset, int count);
 
         var buffer = new byte[1024]; // 1 KB
-        fluxoDoArquivo.Read(buffer, 0, 1024);
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+
+        /* Develuções:
+         * O número total de bytes lidos do buffer. Isso poderá ser menor que o número de bytes solicitado se esse numero de bytes não 
+         * estiver disponivel no momento, ou zero, se o final do fluxo for atingido. */
 
         EscreverBuffer(buffer);
+        // public override int Read(byte[] array, int offset, int count);
 
         Console.ReadLine();
     }
 
     static void EscreverBuffer(byte[] buffer)
     {
-        foreach(byte meuByte in buffer)
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer);
+
+        Console.Write(texto);
+
+        /*foreach(byte meuByte in buffer)
         {
-            Console.Write(meuByte);
+           Console.Write((char)meuByte);
             Console.Write(" ");
-        }
+        }*/
     }
 }
 
